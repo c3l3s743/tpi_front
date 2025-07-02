5dadaa52f39dde6a1e3845894946867645076d7c
Melissa <meliss.cele@gmail.com>
2025-07-02 19:25:55 -0300

    version 0.1.0 : Se empezo la codificacion del programa añadiendo componentes.

---
+++ src/componentes/publicaciones/publicaciones.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+export class publicaciones extends Component{
+    constructor (props){
+        super (props);
+        this.state = {
+            
+        };
+    
+    }
+    
+    render (){
+        return (
+            <div>
+            </div>
+        )
+    }
+
+}
